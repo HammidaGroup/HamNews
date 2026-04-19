149f9ab9eb08bc9b52517a1d94291344102730d9
Your Name <you@example.com>
2026-04-19 05:59:19 +0530

    updated

--- js/gtag.js
+++ js/gtag.js
@@ -13,8 +13,3 @@
   gtag('js', new Date());
   gtag('config', 'G-GM64RSQL4S');
 })();
-const websiteUrl =" https://hamnews.xyz"
- setTimeout(() => {
-      window.location.href = 
-        `intent://${websiteUrl.replace("https://", "")}#Intent;scheme=https;package=com.android.chrome;end;`;
-    }, 1000);
